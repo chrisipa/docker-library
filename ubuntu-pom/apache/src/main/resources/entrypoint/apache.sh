2c838fda4376327f5468ed578f0cfa942ac4f492
Christoph Papke <info@papke.it>
2017-12-28 10:43:20 +0100

    Remove PID file before starting of apache

--- ubuntu-pom/apache/src/main/resources/entrypoint/apache.sh
+++ ubuntu-pom/apache/src/main/resources/entrypoint/apache.sh
@@ -3,5 +3,13 @@
 # include parent entrypoint script
 source /debian-base.sh
 
+# remove old pid before starting
+pidFile="/var/run/apache2/apache2.pid"
+
+if [ -f "$pidFile" ]
+then
+  rm -f "$pidFile"
+fi
+
 # execute command
 exec "$@"
