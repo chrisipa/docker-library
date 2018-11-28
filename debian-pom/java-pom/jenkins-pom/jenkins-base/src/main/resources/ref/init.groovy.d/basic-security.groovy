7db120c6e36eaee515098f60cb19d6035f78283d
Christoph Papke <christoph.papke@kaba.com>
2018-11-28 18:20:44 +0100

    Fix for not overwriting the config.xml if Jenkins is restarted

--- debian-pom/java-pom/jenkins-pom/jenkins-base/src/main/resources/ref/init.groovy.d/basic-security.groovy
+++ debian-pom/java-pom/jenkins-pom/jenkins-base/src/main/resources/ref/init.groovy.d/basic-security.groovy
@@ -3,14 +3,18 @@
 import jenkins.model.*
 import hudson.security.*
 
-def instance = Jenkins.getInstance()
-
 println "--> creating local user 'admin'"
 
+def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+
 hudsonRealm.createAccount('admin','admin')
 instance.setSecurityRealm(hudsonRealm)
-
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
 instance.save()
+
+println "--> delete this script file"
+
+def scriptFile = new File(getClass().protectionDomain.codeSource.location.path)
+scriptFile.delete()
