d91df63ca24e0afc58a7b9263cf314c4de9fcba4
Christoph Papke <christoph.papke@dormakaba.com>
2019-04-27 10:06:27 +0200

    Added images for nodejs and firefox-send

---
+++ debian-pom/node-pom/firefox-send/src/main/resources/firefox-send.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# include parent entrypoint script
+source /node-base.sh
+
+# execute command
+exec "$@"
---
+++ debian-pom/node-pom/node-base/src/main/resources/node-base.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# include parent entrypoint script
+source /debian-base.sh
